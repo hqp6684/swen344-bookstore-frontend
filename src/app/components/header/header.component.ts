6766d5148f29443cb73e8b042664fcfe7129dd23
Huy Pham <huypham@pridetek.com>
2017-03-29 03:15:50 -0400

    check post received hook

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-
   constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
