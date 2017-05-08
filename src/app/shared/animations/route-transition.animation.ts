6cba2a6b2f353a5ebd1fabf8cee111209a90b332
Huy Pham <huypham@pridetek.com>
2017-05-07 23:57:55 -0400

    try 3d

--- src/app/shared/animations/route-transition.animation.ts
+++ src/app/shared/animations/route-transition.animation.ts
@@ -18,13 +18,13 @@ function slideToLeft() {
         state('void', style({ position: 'absolute', width: '100%' })),
         state('*', style({ position: 'absolute', width: '100%' })),
         transition(':enter', [  // before 2.1: transition('void => *', [
-            style({ transform: 'translateX(100%) scale(0)' }),
-            animate('1s ease-in-out', style({ transform: 'translateX(0%) scale(1)' }))
+            style({ transform: 'translate3d(100%, 10%, 0' }),
+            animate('1s ease-in-out', style({ transform: 'translate3d(0, 0, 0)' }))
         ]),
         transition(':leave', [  // before 2.1: transition('* => void', [
-            style({ transform: 'translateX(0%)' }),
+            style({ transform: 'translateX(0%) scale(1)' }),
             animate('1s ease-in-out', style({
-                transform: 'translateX(-100%)',
+                transform: 'translateX(-100%) scale(0)',
 
             }))
         ])
